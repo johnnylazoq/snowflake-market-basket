6022efbba17cd63e0bdceb1c908bb8b88c35945f
Johnny Lazo <johnny.lazoq@gmail.com>
2026-09-11 09:33:05 +0200

    feature: created connection with snowflake

--- src/db.py
+++ src/db.py
@@ -1,1 +1,14 @@
-"""Hadles Snowflake connection"""
+import os
+import snowflake.connector
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_connection():
+    return snowflake.connector.connect(
+        user=os.getenv("SNOWFLAKE_USER"),
+        password=os.getenv("SNOWFLAKE_PASSWORD"),
+        account=os.getenv("SNOWFLAKE_ACCOUNT"),
+        warehouse=os.getenv("SNOWFLAKE_WAREHOUSE"),
+    )
